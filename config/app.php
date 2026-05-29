d82f07401c3478d75940dd95ac21b200e23d4ae6
PAtel18751 <mind59024@gmail.com>
2026-05-29 09:26:33 +0545

    CLeaned noise;

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'name' => 'InvestMatch Nepal',
-    'env' => 'production',
-    'debug' => false,
-    'url' => env('APP_URL', 'https://test.asaancapital.com'),
-    'storage_path' => __DIR__ . '/../storage',
-];
